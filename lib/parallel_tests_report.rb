7bf6b64ba1b9fb1701ae9ee283c8663cb4fecdda
akshatb19 <akshat@amagi.com>
2020-08-17 17:31:53 +0530

    add gemfile and rake task

--- lib/parallel_tests_report.rb
+++ lib/parallel_tests_report.rb
@@ -2,9 +2,7 @@ require "rspec/core"
 require "rspec/core/formatters/base_formatter"
 
 module ParallelTestsReport
-<<<<<<< HEAD
   require 'parallel_tests_report/railtie' if defined?(Rails)
-=======
   class ParallelTestsReport < RSpec::Core::Formatters::BaseFormatter
     RSpec::Core::Formatters.register self, :message, :dump_profile, :seed, :stop, :close
 
--- lib/parallel_tests_report/tasks/generate_report.rake
+++ lib/parallel_tests_report/tasks/generate_report.rake
@@ -2,6 +2,10 @@ require_relative '../../parallel_tests_report/generate_report.rb'
 
 namespace :generate do
   task :report do
+<<<<<<< HEAD
     ParallelTestsReport::GenerateReport.new.start
+=======
+    ParallelTestsReport::GenerateReport.start
+>>>>>>> add gemfile and rake task
   end
 end
